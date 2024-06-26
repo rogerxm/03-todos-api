98219f84ef82c409f0983ad6719346db8649af2e
Roger Montes <rogerxmontes@gmail.com>
2024-06-26 08:35:06 -0600

    todos API

---
+++ src/app/api/todos/[id]/route.ts
@@ -0,0 +1,59 @@
+import prisma from "@/app/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+import * as yup from "yup";
+
+interface Segments {
+  params: {
+    id: string;
+  };
+}
+
+  export async function GET(request: Request, { params }: Segments) {
+  const { id } = params;
+  const todo = await prisma.todo.findFirst({
+    where: { id },
+  });
+
+  if (!todo) {
+    return NextResponse.json(
+      { message: `No se ha encontrado ningun TODO con id ${id}.` },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json(todo);
+}
+
+const putSchema = yup.object({
+  complete: yup.boolean().optional(),
+  description: yup.string().optional(),
+});
+
+export async function PUT(request: Request, { params }: Segments) {
+  const { id } = params;
+  const todo = await prisma.todo.findFirst({
+    where: { id },
+  });
+
+  if (!todo) {
+    return NextResponse.json(
+      { message: `No se ha encontrado ningun TODO con id ${id}.` },
+      { status: 404 }
+    );
+  }
+
+  try {
+    const { complete, description } = await putSchema.validate(
+      await request.json()
+    );
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id },
+      data: { complete, description },
+    });
+
+    return NextResponse.json(updatedTodo);
+  } catch (error) {
+    return NextResponse.json(error, { status: 400 });
+  }
+}
